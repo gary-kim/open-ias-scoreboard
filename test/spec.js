3c0bcd29b590318f574ee96a5148166339d04359
Gary Kim <gary@garykim.dev>
2019-06-21 00:26:55 +0800

    td: add more overdue tests

--- test/spec.js
+++ test/spec.js
@@ -4,10 +4,10 @@ const assert = require('assert');
 const path = require('path');
 const electron = require('electron');
 
-describe('Start Open IAS Scoreboard', function() {
+describe('Open IAS Scoreboard Tests', function () {
     this.timeout(10000);
 
-    beforeEach(function() {
+    before(function () {
         this.app = new Application({
             path: electron,
             args: [path.join(__dirname, '..'), 'testing'],
@@ -18,17 +18,43 @@ describe('Start Open IAS Scoreboard', function() {
         return this.app.start();
     });
 
-    afterEach(function() {
+    afterEach(function () {
+        assert.equal(this.app.isRunning(), true);
+    });
+
+    after(function () {
         if (this.app && this.app.isRunning()) {
             return this.app.stop();
         }
     });
 
-    it('Open Window', function() {
-        return this.app.client.getWindowCount().then(
-            function(count) {
-                assert.equal(count, 1);
-            }
-        );
+    describe('Opening Windows', function() {
+        it('Open Control Board', async function () {
+            assert.equal(await this.app.client.getWindowCount(), 1);
+        });
+        it('Open 1 Scoreboard', async function () {
+            await this.app.client.$('#new-tab-button').click();
+            assert.equal(await this.app.client.getWindowCount(), 2);
+        });
+    
+        it('Open 2 Scoreboards', async function () {
+            await this.app.client.$('#new-tab-button').click();
+            assert.equal(await this.app.client.getWindowCount(), 3);
+        });
     });
+
+    describe('Switching Scoreboards', function () {
+        it('Switch to 1st scoreboard by clicking', async function() {
+            await this.app.client.$(`.tab-button[scoreboard-id="1"]`).click();
+            assert.equal((await this.app.client.$(`.controls[scoreboard-id="1"].hidden`)).state, `failure`);
+            assert.equal((await this.app.client.$(`.controls[scoreboard-id="2"].hidden`)).state, undefined);
+        });
+        it('Switch back to 2nd scoreboard by clicking', async function() {
+            await this.app.client.$(`.tab-button[scoreboard-id="2"]`).click();
+            assert.equal((await this.app.client.$(`.controls[scoreboard-id="1"].hidden`)).state, undefined);
+            assert.equal((await this.app.client.$(`.controls[scoreboard-id="2"].hidden`)).state, `failure`);
+        });
+    });
+    
+
 });
